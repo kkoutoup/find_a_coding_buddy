9f9412daa1ab46a5f8d27c5aff8c262e9cfeef69
Enrica Fedeli <enricafedeli@gmail.com>
2020-08-18 21:44:44 +0100

    changed booking(s) to team(s)

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :teams
   has_many :reviews
   has_many :chatrooms
   has_many :messages, through: :chatrooms
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,4 @@
-class Booking < ApplicationRecord
+class Team < ApplicationRecord
   belongs_to :project
   belongs_to :user
 end
