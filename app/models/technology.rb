0ef0e69875802aef156d98c85f4ee279b231afc8
Enrica Fedeli <enricafedeli@gmail.com>
2020-08-20 12:16:08 +0100

    project model and technology model changed

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -1,6 +1,4 @@
 class Technology < ApplicationRecord
   has_many :projects, through: :project_technologies
-  has_many :project_technologies
   has_many :users, through: :user_technologies
-  has_many :user_technologies
 end
