e82d109e71b2fec8c3bd5b3c63d1bb069b0f8288
Kostas Koutoupis <kkoutoup@gmail.com>
2020-08-20 21:18:50 +0100

    Removed name from chatroom model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_190335) do
+ActiveRecord::Schema.define(version: 2020_08_20_201502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "chatrooms", force: :cascade do |t|
-    t.string "name"
     t.bigint "project_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
