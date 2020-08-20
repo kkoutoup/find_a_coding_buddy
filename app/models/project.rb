ed42558f6114347c078f3e9bcfc217dc4cb33543
Kostas Koutoupis <kkoutoup@gmail.com>
2020-08-20 11:35:58 +0100

    Updating models and schema. Tested in console. Associations seems to be working

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,10 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :teams
+  has_many :users, through: :teams
   has_many :reviews
   has_many :chatrooms
   has_many :messages, through: :chatrooms
+  has_many :project_technologies
   has_many :technologies, through: :project_technologies
 end
