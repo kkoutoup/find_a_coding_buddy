59d797dcf5de03c5cb53778e0a297bee61592174
Kostas Koutoupis <kkoutoup@gmail.com>
2020-08-23 14:54:39 +0100

    added technologies to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
 # Destroy entries
 puts "Destroying existing entries"
 User.destroy_all if Rails.env.development?
+Technology.destroy_all if Rails.env.development?
 
 # Global variables for seeding
-EXPERTISE_LEVEL = ["beginner", "intermediate", "advanced"]
+LEVEL = ["beginner", "intermediate", "advanced"]
 EMAIL_EXTENSIONS = ["@gmail.com", "@yahoo.com", "@hotmail.com", "@business.com", "@aol.com"]
 PERSONAL_SUMMARIES = []
+TECHNOLOGIES = ["Javascript", "Node.js", "express.js", "mongoDB", "Python", "Flask", "Ruby", "Ruby on Rails", "HTML", "CSS/SCSS", "Figma", "C#", ".NET", "PHP", "APIs", "Design", "Android", "Go", "SQL"]
 
 # Populate csv with summaries
-puts "=>Populating personal summaries from csv"
+puts "=> Populating personal summaries from csv"
 require 'csv'
 
 filepath = "profile_descriptions.csv"
@@ -21,7 +23,7 @@ CSV.foreach(filepath, csv_options) do |row|
 end
 
 # Users
-puts "=>Creating users"
+puts "=> Creating users"
 10.times do
   user = User.create!(
     {
@@ -29,15 +31,26 @@ puts "=>Creating users"
       last_name:  Faker::Name.last_name,
       email: Faker::Internet.email,
       personal_summary: PERSONAL_SUMMARIES.sample,
-      expertise_level: EXPERTISE_LEVEL.sample,
+      expertise_level: LEVEL.sample,
       password: "testing"
     }
   )
-  puts "Created user #{user.id}"
 end
 
-puts "=>Updating user email addresses"
+puts "=> Updating user email addresses and passwords"
 User.all.each do |user|
   user.email = "#{user.first_name}_#{user.last_name}#{EMAIL_EXTENSIONS.sample}"
+  user.password = "#{Faker::Verb.past}#{Faker::Superhero.power}#{rand(1..200)}"
   user.save!
 end
+
+# Technologies
+puts "=> Creating technologies"
+TECHNOLOGIES.each do |technology|
+  Technology.create!(
+    {
+      name: technology
+    }
+  )
+end
+
