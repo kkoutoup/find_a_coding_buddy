5bc020d2a61e4bcdbfd23f16913ef7e5a2635a3a
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2020-08-24 21:15:14 +0100

    adding application controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,17 +1,35 @@
 class ApplicationsController < ApplicationController
+  before_action :find_project, except: [:index]
+
+  def index
+    @applications = Application.where(user_id: current_user.id).order(id: :desc)
+  end
+
+  def show; end
+
   def new
+    @application = Application.new(find_project)
   end
 
   def create
+    @application = Application.new(find_project)
+    @application.project = @project
+    @application.user = current_user
+    if @application.save
+      @application.update_attributes(status: nil)
+      redirect_to # project_bookings_path(current_user) TO UPDATE
+    else
+      render :new 
   end
 
   def destroy
+    @application.destroy
   end
 
-  def index
-  end
+  private
 
-  def show
+  def find_project
+    @project = Project.find(params[:project_id])
   end
 
 end
