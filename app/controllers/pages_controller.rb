384929189e8fe32b866a6091589fbb11bd8b12ad
Enrica Fedeli <enricafedeli@gmail.com>
2020-08-31 10:11:23 +0100

    added logic to retrieve and round down members, technologies and projects and styled bar in landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
+require 'rounding'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
     @projects = Project.all
+    @technologiesRoundedDown = Technology.count.floor_to(5)
+    @projectsRoundedDown = Project.count.floor_to(5)
+    @usersRoundedDown = User.count.floor_to(5)
   end
 end
