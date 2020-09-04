83f733a706dd8287b5432d7f28763a256000909f
Enrica Fedeli <enricafedeli@gmail.com>
2020-09-04 08:20:10 +0100

    first attempt to glide

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,6 +1,9 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
 // You can generate new channels where WebSocket features live using the `rails generate channel` command.
 
-import { createConsumer } from "@rails/actioncable"
+import { createConsumer } from "@rails/actioncable";
+import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm'
+
+new Glide('.glide').mount({ Controls, Breakpoints })
 
 export default createConsumer()
