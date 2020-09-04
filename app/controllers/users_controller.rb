160fd40ea4edbe9d19dc8c6d77116ab1e5f73ec9
Enrica Fedeli <enricafedeli@gmail.com>
2020-09-04 21:44:42 +0100

    stated user search, altered profiles, adjusted carousel's bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
+
+    def index
+      if params[:query].present?
+        @users = User.search_by_name_address_and_technologies(params[:query])
+      elsif params.has_key?(:first_name)
+        @users = User.where(first_name: params[:first_name])
+      elsif params.has_key?(:address)
+        @users = User.where(address: params[:address])
+      elsif params.has_key?(:expertise_level)
+        @users = User.where(expertise_level: params[:expertise_level])
+      elsif params.has_key?(:technologies)
+        @users = User.where(technologies: params[:technologies])
+      else
+        @users = User.all
+      end
+    end
+
 
   def show
     @user = User.find(params[:id])
