c429e37b04a12f03add78cfc8538f6554070dab5
Kostas Koutoupis <kkoutoup@gmail.com>
2020-09-14 13:40:27 +0100

    Styling review cards

--- app/helpers/review_helper.rb
+++ app/helpers/review_helper.rb
@@ -1,6 +1,13 @@
 module ReviewHelper
-  def user_info(review)
+
+  def user_initials(review)
+    user = User.find(review.user_id)
+    "#{user.first_name[0]}#{user.last_name[0]}"
+  end
+
+  def user_full_name(review)
     user = User.find(review.user_id)
     "#{ user.first_name } #{ user.last_name }"
   end
+
 end
