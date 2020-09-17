cc2edde7adcc43f943b193667c80ea8c3a03c8b6
Kostas Koutoupis <kkoutoup@gmail.com>
2020-09-17 16:43:27 +0100

    Show project buddie on project owner page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
     path = Rails.root.join("app", "assets", "images", "technologies_icons", "#{ technology.name }.svg")
     File.exist?(path) ? asset_path("technologies_icons/#{ technology.name }.svg") : asset_path("technologies_icons/logo-missing-img.png")
   end
+
+  def render_users(applications)
+    users = applications.map { |application| User.find(application.user_id) }
+    output = ""
+    users.each do |user|
+      output += "<li>#{user.first_name} #{user.last_name}</li>"
+    end
+    output.html_safe
+  end
 end
