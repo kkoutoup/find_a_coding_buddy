153c0e9d1b601e250db784dac2296f6056a41751
Enrica Fedeli <enricafedeli@gmail.com>
2020-09-19 08:02:40 +0100

    more dashboard work

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,4 +2,8 @@ module UserHelper
   def render_personal_summary(user)
     user.personal_summary? ? user.personal_summary.truncate(150, separator: ' ') : "This buddy hasn't filled out their personal summary yet 😢"
   end
+
+  def dashboard_personal_summary(user)
+    user.personal_summary? ? user.personal_summary.truncate(250, separator: ' ') : "To make the most out of Code Buddies remember to add your personal summary"
+  end
 end
