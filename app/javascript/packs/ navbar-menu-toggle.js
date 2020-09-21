41c60cef368d0b070f86b0c957ad387c35fd517d
Kostas Koutoupis <kkoutoup@gmail.com>
2020-09-21 16:05:45 +0100

    Added fa icons to navbar dropdown and resized it

--- app/javascript/packs/ navbar-menu-toggle.js
+++ app/javascript/packs/ navbar-menu-toggle.js
@@ -5,7 +5,7 @@ function navbarMenuToggle() {
   toggle.click(function(){
     var coordinatesTop = toggle.offset().top;
     var coordinatesLeft = toggle.offset().left;
-    menu.css({ top: coordinatesTop + 35, left: coordinatesLeft - 130, right: 0, width: 50  })
+    menu.css({ top: coordinatesTop + 35, left: coordinatesLeft - 130, right: 0, width: 80  })
   })
 }
 
