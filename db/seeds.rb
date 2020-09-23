ff9cadb04e55d7ca93df50a030173d60efcd080c
Kostas Koutoupis <kkoutoup@gmail.com>
2020-09-23 22:04:56 +0100

    More styling bits

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,7 +36,7 @@ CSV.foreach(filepath, csv_options) do |row|
     {
       first_name: Faker::Name.first_name,
       last_name:  Faker::Name.last_name,
-      email: Faker::Internet.email,
+      email: "mymail@job.com",
       personal_summary: row,
       address: CITIES.sample,
       expertise_level: LEVEL.sample,
